6cb360dded37565efbd75d39528f63c4c696cad6
Luke Terwilliger <66847110+lterwilliger@users.noreply.github.com>
2022-03-10 11:55:26 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,7 @@ $(function () {
     "https://gist.github.com/barrycumbie/e2491ce65ed1b8711ce78fc897028597"
   ];
   
+  
   $.each(classCharlies, function (index, value) {
     $("#cardOutput").append(
       '  <div class="card game-card"> <div class="card-header">Game No. ' +
@@ -17,8 +18,29 @@ $(function () {
         value +
         "</a>  </div></div> "
     );
-  });
-  
+
+  var fs = require('fs');
+  var text = fs.readFileSync("./repositoryList.txt", 'utf-8');
+  var repositoryList = text.split('\n')
+  var reposLength = repositoryList.length;
+  // Code to read text file into gameList array
+  var fileS = require('fs');
+  var stringAll = fileS.readFileSync("./gameList.txt", 'utf-8');
+  var gameList = stringAll.split('\n')
+  var gameLength = gameList.length;
+
+  // Game output from gameList.txt
+  $.each(repositoryList, function (index, value) {
+      $("#RepositoryOutput").append(
+         '<a class="dropdown-item" href="https://github.com/' + value + '">' + value + '</a>'
+      //  `<a class="dropdown-item" href="https://github.com/` ${ value } `"> ` value + '</a>'
+      );
+  }
+  $.each(gameList, function (index, value) {
+      $("#gameOutput").append(
+        '<a class="dropdown-item" href="https://github.com/' + value + '">'+ value + '</a>'
+      );
+  }
   // need functions to make row or column per x amount of cards
 
 });
