0b22d09aa18501fdb4e918b172a6d445504a0918
Andrew Sutton <97402701+errorless-stone@users.noreply.github.com>
2022-03-10 12:14:00 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,31 @@
 $(function () {
   console.log("ready!");
+
   const classCharlies = [
     "https://barrycumbie.github.io/legendary-waddle/",
     "https://codepen.io/barrycumbie/pen/GROaGBQ",
+    "https://gist.github.com/barrycumbie/e2491ce65ed1b8711ce78fc897028597",
+    "https://gist.github.com/barrycumbie/e2491ce65ed1b8711ce78fc897028597",
+    "https://gist.github.com/barrycumbie/e2491ce65ed1b8711ce78fc897028597",
+    "https://gist.github.com/barrycumbie/e2491ce65ed1b8711ce78fc897028597",
     "https://gist.github.com/barrycumbie/e2491ce65ed1b8711ce78fc897028597"
+
   ];
 
+  // const classCharlies = $.parseJSON("/gameList.json")
+  
   $.each(classCharlies, function (index, value) {
+    index+=1
     $("#cardOutput").append(
-      '  <div class="card game-card"> <div class="card-header">Game No. ' +
+      '  <div class="col-sm-4 col-lg-3 game-card-col" > <div class="card game-card"> <div class="card-header">Game No. ' +
         index +
         '</div> <div class="card-body">    <a href="' +
         value +
         '">' +
         value +
-        "</a>  </div></div> "
+        "</a>  </div></div> </div>"
     );
   });
   
-  // need functions to make row or column per x amount of cards
+
 });
