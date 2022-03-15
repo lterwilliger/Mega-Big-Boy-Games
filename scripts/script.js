0b11a3e91ccaee018e461a19f3fd9ed7bca1b4ef
laurenmalone01 <97190412+laurenmalone01@users.noreply.github.com>
2022-03-15 11:45:41 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,186 @@
-import { games } from "./peoplecode.js";
+games = [
+  {
+    title: "Charlie Wow",
+    link: "https://colintate24.github.io/lab-charlie/",
+    author: "Colin Tate",
+  },
+  {
+    title: "Cool Name Facts",
+    link: "https://colintate24.github.io/scaling-tribble/",
+    author: "Colin Tate",
+  },
+  {
+    title: "Da Name Game",
+    link: "https://garner-jaiden.github.io/dev-lab-delta/",
+    author: "Jaiden Garner",
+  },
+  {
+    title: "Many Wow Happy Doge",
+    link: "https://garner-jaiden.github.io/dev_lab_charlie/",
+    author: "Jaiden Garner",
+  },
+  {
+    title: "Khoi's Wow",
+    link: "https://khoiuna.github.io/khoi_wow_meme/",
+    author: "Khoi Nguyen",
+  },
+  {
+    title: "Echo Lab",
+    link: "https://khoiuna.github.io/cis-echo-lab/",
+    author: "Khoi Nguyen",
+  },
+  {
+    title: "Professor-Doge",
+    link: "https://almostheaven52.github.io/Professor-Doge/",
+    author: "Zach Newell",
+  },
+  {
+    title: "Dogey-Howser",
+    link: "https://almostheaven52.github.io/dogey-howser/",
+    author: "Zach Newell",
+  },
+  {
+    title: "Delta",
+    link:  "https://natalie-daniel.github.io/DELTA/",
+    author: "Natalie Daniel",
+  },
+  {
+    title: "WowGame",
+    link:  "https://natalie-daniel.github.io/wowgame/",
+    author: "Natalie Daniel",
+  },
+  {
+    title: "Studious Waffle",
+    link:  "https://studentzilla.github.io/studious-waffle/",
+    author: "StudentZilla",
+  },
+  {
+    title: "Fuzzy Computing Machine",
+    link:  "https://cdman814.github.io/fuzzy-computing-machine/",
+    author: "CDman814",
+  },
+  {
+    title: "Vigalante Octo Parakeet",
+    link:  "https://cdman814.github.io/vigilant-octo-parakeet/",
+    author: "CDman814",
+  },
+  {
+    title: "FrostGames",
+    link:  "https://frosty9424.github.io/frostgames/",
+    author: "Frosty9424",
+  },
+  {
+    title: "Upgraded Bassoon",
+    link:  "https://gatordj98.github.io/upgraded-bassoon/",
+    author: "GatorDJ98",
+  },
+  {
+    title: "Symmetrical Fiesta",
+    link:  "https://gatordj98.github.io/symmetrical-fiesta/",
+    author: "GatorDJ98",
+  },
+  {
+    title: "Friendly Computing Machine",
+    link:  "https://lterwilliger.github.io/friendly-computing-machine/",
+    author: "Lterwilliger",
+  },
+  {
+    title: "Urban Train",
+    link:  "https://lterwilliger.github.io/urban-train/",
+    author: "Lterwilliger",
+  },
+  {
+    title: "Shiny Shinee",
+    link:  "https://laurenmalone01.github.io/shiny-shinee/",
+    author: "BigDaddy Malone",
+  },
+  {
+    title: "Upgraded Memory",
+    link:  "https://laurenmalone01.github.io/upgraded-memory/",
+    author: "BigDaddy Malone",
+  },
+  {
+    title: "Edgy Bourban",
+    link:  "https://errorless-stone.github.io/edgy-bourbon/",
+    author: "Errorless-Stone",
+  },
+  {
+    title: "Cumbie-Delta",
+    link:  "https://errorless-stone.github.io/cumbie-delta/",
+    author: "Errorless-Stone",
+  },
+  {
+    title: "Extinction-Party",
+    link: "https://akennemer.github.io/extinction-party/",
+    author: "Ashe Kennemer",
+  },
+  {
+    title: "Teen Idle",
+    link: "https://akennemer.github.io/teen-idle/",
+    author: "Ashe Kennemer",
+  },
+  {
+    title: "CharlieCode",
+    link: "https://cgooch99.github.io/CharlieCode/",
+    author: "Cgooch99",
+  },
+  {
+    title: "Juicy Fruit",
+    link: "https://cgooch99.github.io/juicy-fruit/",
+    author: "Cgooch99",
+  },
+  {
+    title: "LabCharlie",
+    link: "https://peytonjunjulas.github.io/labCharlie/",
+    author: "PeytonJunjulas",
+  },
+  {
+    title: "Lab Delta",
+    link: "https://peytonjunjulas.github.io/lab-Delta/",
+    author: "PeytonJunjulas",
+  },
+  {
+    title: "Raney Lab Echo",
+    link: "https://gavinraney.github.io/raney-lab-echo/",
+    author: "Gavin Raney",
+  },
+  {
+    title: "Raney Lab Delta",
+    link: "https://gavinraney.github.io/raney-lab-delta/",
+    author: "Gavin Raney",
+  },
+  {
+    title: "Scaling Palm Tree",
+    link: "https://kobyingram11.github.io/scaling-palm-tree/",
+    author: "KobyIngram11",
+  },
+  {
+    title: "Expert Computing Machine",
+    link: "https://jacobsanders7.github.io/expert-computing-machine/",
+    author: "Jacobsanders7",
+  },
+  {
+    title: "Crispy Telegram",
+    link: "https://jacobsanders7.github.io/crispy-telegram/pages/charlie-wow.html",
+    author: "Jacobsanders7",
+  },
+];
 
 $(function () {
   // iterate over the JSON array
   $.each(games, function (index, item) {
     // create a brand new HTML element JUST with code
-    var el = `<div class='card text-center mx-auto bg-dark blue' style="width: 200px"> 
-  <button class='cardOpen2 btn btn-outline-info'> ${item.title} </button> 
-   <div class='card'> 
+    var el = `<div class='card text-center mx-auto bg-light blue' style="width: 200px">
+  <button class='cardOpen2 btn btn-outline-primary'> ${item.title} </button> 
+   <div class='card'>
        <br> 
        <a href="${item.link}" target="_blank" class="link-info" >GAME LINK</a> 
-       <p class="text-white">Create by: ${item.author} </p> 
+       <p class="text-black">Created by: ${item.author} </p> 
        <br> 
-       <button class='btn btn-outline-info cardClose2'>Close</button> 
-   </div>
+          </div>
  </div>`;
 
     $("#cardOutput").append(el);
- 
+    // $(".nav-dropdown").append($(`<li><a href="#!"> ${item.author} </a></li>`));
   });
 });
