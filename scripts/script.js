309154e710b22a828643ce4c6ea5d8d20ac50bdc
laurenmalone01 <97190412+laurenmalone01@users.noreply.github.com>
2022-03-15 11:07:18 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,21 @@
+import { games } from "./config.js";
+
 $(function () {
-  console.log("ready!");
+  // iterate over the JSON array
+  $.each(games, function (index, item) {
+    // create a brand new HTML element JUST with code
+    var el = `<div class='card text-center mx-auto bg-dark blue' style="width: 200px"> \
+  <button class='cardOpen2 btn btn-outline-info'> ${item.title} </button> \
+   <div class='cardHide'> \
+       <br> \
+       <a href="${item.link}" target="_blank" class="link-info" >GAME LINK</a> \
+       <p class="text-white">Create by: ${item.author} </p> \
+       <br> \
+       <button class='btn btn-outline-info cardClose2'>Close</button> \
+   </div>
+ </div>`;
 
-  const classCharlies = [
-    "https://barrycumbie.github.io/legendary-waddle/",
-    "https://codepen.io/barrycumbie/pen/GROaGBQ",
-    "https://gist.github.com/barrycumbie/e2491ce65ed1b8711ce78fc897028597",
-    "https://gist.github.com/barrycumbie/e2491ce65ed1b8711ce78fc897028597",
-    "https://gist.github.com/barrycumbie/e2491ce65ed1b8711ce78fc897028597",
-    "https://gist.github.com/barrycumbie/e2491ce65ed1b8711ce78fc897028597",
-    "https://gist.github.com/barrycumbie/e2491ce65ed1b8711ce78fc897028597"
-
-  ];
-
-  // const classCharlies = $.parseJSON("/gameList.json")
-  
-  $.each(classCharlies, function (index, value) {
-    index+=1
-    $("#cardOutput").append(
-      '  <div class="col-sm-4 col-lg-3 game-card-col" > <div class="card game-card"> <div class="card-header">Game No. ' +
-        index +
-        '</div> <div class="card-body">    <a href="' +
-        value +
-        '">' +
-        value +
-        "</a>  </div></div> </div>"
-    );
+    $("#cards").append(el);
+    $(".nav-dropdown").append($(`<li><a href="#!"> ${item.author} </a></li>`));
   });
-  
-
 });
