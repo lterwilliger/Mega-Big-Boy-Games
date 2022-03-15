9973dbe3aa19e000584cfc9c14ec4935d553ad45
Luke Terwilliger <66847110+lterwilliger@users.noreply.github.com>
2022-03-15 11:32:23 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,14 @@ $(function () {
   // iterate over the JSON array
   $.each(games, function (index, item) {
     // create a brand new HTML element JUST with code
-    var el = `<div class='card text-center mx-auto bg-dark blue' style="width: 200px"> \
-  <button class='cardOpen2 btn btn-outline-info'> ${item.title} </button> \
-   <div class='cardHide'> \
-       <br> \
-       <a href="${item.link}" target="_blank" class="link-info" >GAME LINK</a> \
-       <p class="text-white">Create by: ${item.author} </p> \
-       <br> \
-       <button class='btn btn-outline-info cardClose2'>Close</button> \
+    var el = `<div class='card text-center mx-auto bg-dark blue' style="width: 200px"> 
+  <button class='cardOpen2 btn btn-outline-info'> ${item.title} </button> 
+   <div class='cardHide'> 
+       <br> 
+       <a href="${item.link}" target="_blank" class="link-info" >GAME LINK</a> 
+       <p class="text-white">Create by: ${item.author} </p> 
+       <br> 
+       <button class='btn btn-outline-info cardClose2'>Close</button> 
    </div>
  </div>`;
 
